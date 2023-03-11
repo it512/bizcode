d3aff4d51a18c5bfb93aeb12d09b0ac8554dadfe
it512 <it512@msn.com>
2023-03-11 17:17:21 +0800

    main

--- cmd/code/main.go
+++ cmd/code/main.go
@@ -9,11 +9,7 @@ import (
 func main() {
 	c := bizcode.By("0A")
 
-	for {
-		code := c()
-		c29 := code[29]
-		if c29 == '0' {
-			fmt.Printf("code = %s, type = %s\n", code, bizcode.CodeType(code))
-		}
-	}
+	code := c()
+
+	fmt.Printf("code = %s, type = %s\n", code, bizcode.CodeType(code))
 }
